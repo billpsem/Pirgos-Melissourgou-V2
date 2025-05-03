2468c36629aabfba143e2149af21e468106b00a1
billpsem <billpsem@gmail.com>
2025-05-03 03:36:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+  // // Hero image slider
+  // const heroSection = document.querySelector('.hero');
+  // const images = [
+  //   './images/heroimage2.jpg',
+  //   './images/heroimage3.jpg',
+  //   './images/heroimage4.jpg'
+  // ];
+  // let currentIndex = 0;
+  // setInterval(() => {
+  //   currentIndex = (currentIndex + 1) % images.length;
+  //   heroSection.style.backgroundImage = `url('${images[currentIndex]}')`;
+  // }, 4000);
+
+  
+  // web3forms scripts
+  window.onload = function() {
+        // Reset the form fields when the page loads
+        document.getElementById("form").reset();
+  };
